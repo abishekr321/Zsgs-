a9ed59396f924f30d96889d4c5b77c463b6319f5
ABISHEK R <86834196+abishekr321@users.noreply.github.com>
2023-02-27 10:42:51 +0530

    Add files via upload

---
+++ ThirukuralApp/src/main/java/com/zsgs/thirukuralapplication/homepage/HomePageControllerCallBack.java
@@ -0,0 +1,11 @@
+package com.zsgs.thirukuralapplication.homepage;
+
+public interface HomePageControllerCallBack {
+
+	void chooseOption(String option);
+
+	void fetchSpeacialities();
+
+	void fetchHistory();
+
+}
